72fe631ba8bbb9f79bfb076af16cb88c68407f97
Larry LaMar <lmrlrry@gmail.com>
2020-11-03 23:19:39 -0600

    completed registration data

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/dto/RegisterFormDTO.java
@@ -0,0 +1,29 @@
+package org.launchcode.javawebdevtechjobsauthentication.models.dto;
+
+import org.launchcode.javawebdevtechjobsauthentication.models.User;
+import org.launchcode.javawebdevtechjobsauthentication.models.data.UserRepository;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+
+
+public class RegisterFormDTO extends LoginFormDTO {
+
+    private UserRepository userRepository;
+
+    private String verifyPassword;
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
+
+}
